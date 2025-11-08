3f84ef99768101e0dccf05704f42855e3447c873
Alexander <1322472@mail.ru>
2025-11-08 20:51:34 +0300

    web_app first day

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
+
+
+app = FastAPI(title="Моё первое Web приложение")
+
+@app.get("/")
+def home():
+    return "Hello World"
+
+
+@app.get("/hello")
+def html_hello():
+    html = """
+    <html>
+        <head>
+        <title>Моё первое Web приложение</title>
+        </head>
+        <body>
+            <h1>Hello World</h1>
+            <p>Наша первая страница</p>
+            <a href="http://localhost:8000/file">На страницу File</a>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html)
+
+@app.get("/file")
+def from_file():
+    with open("templates/index.html", "r", encoding="utf-8") as fl:
+        content = fl.read()
+    return HTMLResponse(content=content) 
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True) #reload=True сервет автоматически перезагружается при изменении кода, при рабочем варианте - убираем.
